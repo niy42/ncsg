5fee5c5d18271dcdc5dd30a7c6d1c73aaa5060fe
niy42 <obanlaniyi42@gmail.com>
2024-11-11 15:31:58 +0100

    modified next config

---
+++ out1/assets/index.js
@@ -0,0 +1,17 @@
+import food1 from '../assets/food1.jpg';
+import group from '@/public/assets/group.jpg';
+import onions from '../assets/onions.jpg';
+import group1 from '@/public/assets/group1.jpg';
+import strawb from '../assets/strawb.jpg';
+import ncsg1 from '@/public/assets/ncsg1.jpg';
+import ncsg3 from '../assets/ncsg3.jpg';
+
+export {
+    food1,
+    group,
+    onions,
+    group1,
+    strawb,
+    ncsg1,
+    ncsg3
+}
