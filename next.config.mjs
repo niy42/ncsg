1930c9a9f8ba90774a01acbb4778ac6ea2bc025b
niy42 <obanlaniyi42@gmail.com>
2024-11-11 14:32:27 +0100

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { withNextVideo } from "next-video/process";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: 'export',  // Tells Next.js to prepare the app for static export
+    // output: 'export',  // Tells Next.js to prepare the app for static export
     trailingSlash: true,  // Optional: Adds trailing slashes to URLs (e.g., /about/ instead of /about)
 };
 
